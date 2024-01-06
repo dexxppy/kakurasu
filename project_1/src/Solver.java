f893aa6c400e9c7d7b12446ba420c05a67caa8f6
48884 <01187005@pw.edu.pl>
2024-01-06 15:44:16 +0100

    solver in progress

--- project_1/src/Solver.java
+++ project_1/src/Solver.java
@@ -6,13 +6,10 @@ public class Solver {
     private List<List<List<Integer>>> optionsForEachRow;
     private List<List<List<Integer>>> optionsForEachColumn;
 
-    public static List<Integer> emptyFiller = new ArrayList<>();
-
     public Solver(int[] rowsSum, int[] columnSum){
         this.board = new Board(rowsSum, columnSum);
         this.optionsForEachRow = new ArrayList<>();
         this.optionsForEachColumn = new ArrayList<>();
-        emptyFiller.add(0);
         setAllOptions();
     }
 
@@ -77,8 +74,7 @@ public class Solver {
                     System.out.println();
                     System.out.println(optionsForEachColumn);
                 }
-                optionsForEachRow.get(i).remove(0);
-                optionsForEachRow.get(i).add(emptyFiller);
+                addEmptyFiller(i, true);
                 System.out.println("2");
                 printBoard();
                 System.out.println(optionsForEachRow);
@@ -103,8 +99,7 @@ public class Solver {
                     System.out.println(optionsForEachColumn);
                 }
                 System.out.println("2");
-                optionsForEachColumn.get(i).remove(0);
-                optionsForEachColumn.get(i).add(emptyFiller);
+                addEmptyFiller(i, false);
                 printBoard();
                 System.out.println(optionsForEachRow);
                 System.out.println();
@@ -136,11 +131,24 @@ public class Solver {
             }
         }
 
+        for(int i = 1; i <= board.getSize(); i++){
+            if(columnSolved(i)){
+                if(!board.getCell(i, targetRow+1).isFilled()){
+                    for(List<Integer> rowOptions : optionsForEachRow.get(targetRow)){
+                        if(rowOptions.contains(i)){
+                            optionsToRemove.add(rowOptions);
+                        }
+                    }
+                }
+            }
+        }
+
         for(List<Integer> option : optionsToRemove){
             optionsForEachRow.get(targetRow).remove(option);
         }
 
     }
+
 
     public void removeUnreachableOptionsForColumn(Cell cellTicked){
         int necessaryElement = cellTicked.getCoordinateY();
@@ -159,6 +167,18 @@ public class Solver {
 
             if(!containsNecessaryElement){
                 optionsToRemove.add(option);
+            }
+        }
+
+        for(int i = 1; i <= board.getSize(); i++){
+            if(rowSolved(i)){
+                if(!board.getCell(targetColumn+1, i).isFilled()){
+                    for(List<Integer> columnOptions : optionsForEachColumn.get(targetColumn)){
+                        if(columnOptions.contains(i)){
+                            optionsToRemove.add(columnOptions);
+                        }
+                    }
+                }
             }
         }
 
@@ -224,7 +244,7 @@ public class Solver {
             }
 
             for (List<Integer> option : optionsToRemove) {
-                optionsForEachRow.get(columnIndex).remove(option);
+                optionsForEachColumn.get(columnIndex).remove(option);
             }
 
             columnIndex++;
@@ -235,34 +255,249 @@ public class Solver {
     public void clearUnreachableOptions(){
         for(int i=0; i < board.rowsSum.length; i++){
             if(board.rowsSum[i] == 0){
-                optionsForEachRow.get(i).remove(0);
-                optionsForEachRow.get(i).add(emptyFiller);
+                addEmptyFiller(i, true);
                 removeUnreachableOptionsForColumns(i);
             }
 
             if(board.columnsSum[i] == 0){
-                optionsForEachColumn.get(i).remove(0);
-                optionsForEachColumn.get(i).add(emptyFiller);
+                addEmptyFiller(i, false);
                 removeUnreachableOptionsForRows(i);
             }
         }
     }
 
-    public void solve(){
-        clearUnreachableOptions();
+    public void addEmptyFiller(int index, boolean isRow){
 
-        while(!board.isCompleted()) {
+        List<Integer> emptyFiller = new ArrayList<>();
+        emptyFiller.add(0);
+
+        if(isRow){
+
+            while(!optionsForEachRow.get(index).isEmpty()){
+                optionsForEachRow.get(index).remove(0);
+            }
+
+            optionsForEachRow.get(index).add(emptyFiller);
+        }else{
+
+            while(!optionsForEachColumn.get(index).isEmpty()) {
+                optionsForEachColumn.get(index).remove(0);
+            }
+
+            optionsForEachColumn.get(index).add(emptyFiller);
+        }
+
+    }
+
+    public void clearCompleted(){
+
+        for(int i = 0; i < board.getSize(); i++){
+            if(rowSolved(i+1)){
+                addEmptyFiller(i, true);
+            }
+
+            if(columnSolved(i+1)){
+                addEmptyFiller(i, false);
+            }
+        }
+    }
+
+    public void easySolve(){
+        clearUnreachableOptions();
+        System.out.println(optionsForEachRow);
+        System.out.println();
+        System.out.println(optionsForEachColumn);
+
+        while(!solvingSuccessful()) {
+            if(!easilySolvable() || !isSolvable()){
+                return;
+            }
             tickNecessaryCells();
+            clearCompleted();
             printBoard();
             System.out.println(optionsForEachRow);
             System.out.println();
             System.out.println(optionsForEachColumn);
         }
 
-        System.out.println("Completed: " + board.isCompleted());
+        System.out.println("Completed: " + solvingSuccessful());
 
     }
 
+    public boolean solvingSuccessful(){
+        return board.isCompleted();
+    }
+
+    // index > 0
+    public boolean rowSolved(int index){
+
+        int sum = 0;
+
+        for(int i = 1; i <= board.size; i++){
+            if(board.getCell(i, index).isFilled()){
+                sum += i;
+            }
+        }
+
+        return sum == board.rowsSum[index-1];
+    }
+
+    // index > 0
+    public boolean columnSolved(int index){
+
+        int sum = 0;
+
+        for(int i = 1; i <= board.size; i++){
+            if(board.getCell(index, i).isFilled()){
+                sum += i;
+            }
+        }
+
+        return sum == board.columnsSum[index-1];
+    }
+
+    public boolean isSolvable() {
+
+        for (int i = 1; i <= board.getSize(); i++) {
+
+            if (!rowSolved(i) && optionsForEachRow.get(i - 1).get(0).get(0) == 0) {
+                return false;
+            } else if (rowSolved(i) && optionsForEachRow.get(i - 1).get(0).get(0) != 0) {
+                return false;
+            }
+
+            if (!columnSolved(i) && optionsForEachColumn.get(i - 1).get(0).get(0) == 0) {
+                return false;
+            } else if (columnSolved(i) && optionsForEachColumn.get(i - 1).get(0).get(0) != 0) {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    public boolean easilySolvable(){
+
+        for(List<List<Integer>> rowOptions : optionsForEachRow){
+            if(rowOptions.size() == 1){
+                return true;
+            }
+        }
+
+        for(List<List<Integer>> columnOptions : optionsForEachColumn){
+            if(columnOptions.size() == 1){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void removeUnneededRowOptions(int index, List<Integer> baseOption){
+
+        List<List<Integer>> optionsToRemove = new ArrayList<>();
+
+        for(List<Integer> option : optionsForEachRow.get(index)){
+            if(!option.equals(baseOption)){
+                optionsToRemove.add(option);
+            }
+        }
+
+        for(List<Integer> option : optionsToRemove){
+            optionsForEachRow.get(index).remove(option);
+        }
+    }
+
+    public void removeUnneededColumnOptions(int index, List<Integer> baseOption){
+
+        List<List<Integer>> optionsToRemove = new ArrayList<>();
+
+        for(List<Integer> option : optionsForEachColumn.get(index)){
+            if(!option.equals(baseOption)){
+                optionsToRemove.add(option);
+            }
+        }
+
+        for(List<Integer> option : optionsToRemove){
+            optionsForEachColumn.get(index).remove(option);
+        }
+    }
+
+    public void tryEachOption(){
+
+        Board tempBoard = this.board;
+        List<List<List<Integer>>> tempOptionsForEachRow = this.optionsForEachRow;
+        List<List<List<Integer>>> tempOptionsForEachColumn = this.optionsForEachColumn;
+
+        int rowIndex = 0;
+        for(List<List<Integer>> rowOptions : optionsForEachRow){
+
+            for(List<Integer> option : rowOptions){
+
+                removeUnneededRowOptions(rowIndex, option);
+                easySolve();
+
+                if(solvingSuccessful()){
+                    return;
+                }else{
+                    this.board = tempBoard;
+                    this.optionsForEachRow = tempOptionsForEachRow;
+                    this.optionsForEachColumn = tempOptionsForEachColumn;
+                }
+
+            }
+
+            rowIndex++;
+        }
+
+        int columnIndex = 0;
+        for(List<List<Integer>> columnOptions : optionsForEachColumn){
+
+            for(List<Integer> option : columnOptions){
+
+                removeUnneededColumnOptions(columnIndex, option);
+                easySolve();
+
+                if(solvingSuccessful()){
+                    return;
+                }else{
+                    this.board = tempBoard;
+                    this.optionsForEachRow = tempOptionsForEachRow;
+                    this.optionsForEachColumn = tempOptionsForEachColumn;
+                }
+
+            }
+
+            columnIndex++;
+        }
+    }
+
+    public void solve(){
+        printBoard();
+        clearUnreachableOptions();
+
+        if(easilySolvable()){
+            System.out.println("Easy solve-------------------------------------------");
+            easySolve();
+        }else{
+            printBoard();
+            System.out.println(optionsForEachRow);
+            System.out.println();
+            System.out.println(optionsForEachColumn);
+            while(!solvingSuccessful()) {
+                System.out.println("every option-------------------------------------------");
+                tryEachOption();
+            }
+        }
+
+        if(solvingSuccessful()){
+            System.out.println(this.board);
+        }else{
+            System.out.println("Nie udało się rozwiązać planszy");
+        }
+
+    }
 
 
     public static void main(String[] args){
@@ -274,10 +509,6 @@ public class Solver {
         Board board3 = generator3.getBoard();
         Solver solver1 = new Solver(board1.rowsSum, board1.columnsSum);
         solver1.solve();
-//        Solver solver2 = new Solver(board2.rowsSum, board2.columnsSum);
-//        solver2.solve();
-//        Solver solver3 = new Solver(board3.rowsSum, board3.columnsSum);
-//        solver3.solve();
     }
 
 }
